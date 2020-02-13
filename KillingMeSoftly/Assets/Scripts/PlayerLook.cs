090e8e932cb49ce37e68d48b6d611468466bb775
Petra Frantz <petra091@gmail.com>
2020-02-13 22:41:51 +0000

    basic boi

---
+++ KillingMeSoftly/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSens = 100f;
+
+    public Transform leBody;
+
+    float xRot = 0f;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        leBody.Rotate(Vector3.up * mouseX);
+    }
+}
